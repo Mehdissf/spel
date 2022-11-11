3d3016d583e591d0a69f128acfc51f55e9257c4c
Mahdi <mahdi.yousefi.skola.taby.se>
2022-11-11 12:49:07 +0100

    test

--- Äventyrsspel.py
+++ Äventyrsspel.py
@@ -25,14 +25,11 @@ Välj en av dörrarna nedan för att gå vidare:
 
 """)
 
-dörrar()
 def meny():
     # time.sleep(0.75)  Vi måte fördröja fraserna
-    meny_alternativ = ["1", "2", "3"]
-    Players_val = ""
-    while Players_val not in meny_alternativ:
-
-        cls()
+    while True: #Kolla om spelaren leveer:
+        meny_alternativ = ["1", "2", "3"]
+        Players_val = ""
         print('''Hej och välkomna till äventyrsspelet skapat av Mahdi och Elliot!! \n  
 
     #### Välj ett av de alternativen nedan ####
@@ -42,46 +39,53 @@ def meny():
             3) Hjälp
             
             ''')
+        while Players_val not in meny_alternativ:
+            Players_val = input("\n \n Ange ditt alternativ här: ")
+            if Players_val not in meny_alternativ:
+                print("""
+                Skriv in rätt tack!!!
+                """)
+                meny()
+                
 
-        
-        Players_val = input("\n \n Ange ditt alternativ här: ")
-        # print("Du har valt " + Players_val)
-        if Players_val =="1":
-            startsida_alternativ = ["1", "2"]
-            Players_val2 = ""
-            while Players_val2 not in startsida_alternativ:  #vi kan också skapa en ny funkttion för startsidan istället för att skriva allt här i alternativ 1)
+            # print("Du har valt " + Players_val)
+            
+            if Players_val =="1":
+                startsida_alternativ = ["1", "2"]
+                Players_val2 = ""
+                while Players_val2 not in startsida_alternativ:  #vi kan också skapa en ny funkttion för startsidan istället för att skriva allt här i alternativ 1)
+                    cls()
+                    print("""       
+            ########
+            Det är nu sommaren år 2050.                                                                            
+            Du och din familj för att ha det kul och gå på semester, 
+            åker till Borås för att besöka Elliot som har sedan år 2030 varit bosatt i Borås.                              
+            Han bor tillsammans med sina tre moderlösa barn. 
+            Informantion om vad det är som hänt med barnens mödrar är ett mysterium och ingen har än idag förstått det. 
+            Legender säger att Elliot har aldrig haft någon fru.... Men hur fick han de där tre barnen då??? 
+            Ingen vet det.. Skitsamma låt oss gå vidare nu..... 
+            ######### 
+
+            ####### 
+            Välj ett alternativ: 
+            #######
+                1) Fortsätt 
+                2) Tillbaka till meny
+
+                        """)
+                    Players_val2 = input("\n\n Ange ditt alternativ här: ")
+                    if Players_val2 == "1":
+                        print(dörrar())
+                    if Players_val2 =="2":
+                        meny()
+
+            if Players_val == "3":
+                hjälplistan()
+            if Players_val == "2":
                 cls()
-                print("""       
-        ########
-        Det är nu sommaren år 2030.                                                                            
-        Du och din familj för att ha det kul och gå på semester, 
-        åker till Borås för att besöka Elliot som har sedan år 2025 varit bosatt i Borås.                              
-        Han bor tillsammans med sina tre moderlösa barn. 
-        Informantion om vad det är som hänt med barnens mödrar är ett mysterium och ingen har än idag förstått det. 
-        Legender säger att Elliot har aldrig haft någon fru.... Men hur fick han de där tre barnen då??? 
-        Ingen vet det.. Skitsamma låt oss gå vidare nu..... 
-        ######### 
-
-        ####### 
-        Välj ett alternativ: 
-        #######
-            1) Fortsätt 
-            2) Tillbaka till meny
-
-                    """)
-                Players_val2 = input("\n\n Ange ditt alternativ här: ")
-                if Players_val2 == "1":
-                    print(dörrar())
-                if Players_val2 =="2":
-                    meny()
-
-        if Players_val == "3":
-            hjälplistan()
-        if Players_val == "2":
-            cls()
-            print("Hej dååå!!")       
-            break 
-        
+                print("Hej dååå!!")       
+                break 
+            
 meny()
 
 
