c50e27dee791cbce0f61f019f09c98b89bcdc227
Elliot <elliot.bruno@skola.taby.se>
2023-01-13 13:13:11 +0100

    Ändringar

--- Äventyrsspel/Äventyrsspel.py
+++ Äventyrsspel/Äventyrsspel.py
@@ -2,7 +2,7 @@ import random
 import time
 import os
 import sys
-# from Items import *
+from Items import *
 # from Loot import *
 from Butik import *
 from Player import *
@@ -22,15 +22,37 @@ def falla_scen(Player_1):
 def strid_scen(Player_1):
     Player.strid(Player_1)
 
-def kista_scen():
+def kista_scen(Player_1):
     os.system('cls')
     print("""
 =======================================    
 Du har hittat en kista!!
-Tryck [Enter] för att fortsätta till kistan!
+Kistan innehåller tre st olika föremål
+Välj ett föremål:
+1) 2) 3)
 =======================================
     """)
-    input() #tillfällig kod
+    kista = [Slangbella, Kannon, Gremlin]
+    val=input()
+    if val == "1":
+        os.system('cls')
+        print("Du fick en Slangbella")
+        input("Vill du behålla den? 1) Absolut 2)Nej skit på dig")
+        if val == "1":
+            Player_1.inventory.append(Slangbella)
+        elif val == "2":
+            os.system('cls')
+    elif val =="2":
+        os.system('cls')
+        print("I kistan hittade du en kannon")
+    elif val =="3":
+        os.system('cls')
+        print("Du hittade en Gremlin, var försiktig")
+    
+
+# for item in kista: print(item.namn)
+
+    # input() #tillfällig kod
 
 def dörrar(Player_1):
     
