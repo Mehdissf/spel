593059da5d16963eaa667931c7f61babf05fcf86
Elliot <elliot.bruno@skola.taby.se>
2023-01-13 13:13:29 +0100

    ad

--- Äventyrsspel/Items.py
+++ Äventyrsspel/Items.py
@@ -8,7 +8,13 @@ svärd = Item("Svärd", 10)
 spjut = Item("Spjut", 10)
 pilbåge = Item("Pilbåge",8)
 sten = Item("Sten", 2)
-
+Slangbella=Item("Slangbella",3)
+Kannon=Item("Kannon",5)
+Gremlin=Item("Gremlin",7)
 
 lista =[svärd, spjut, pilbåge, sten]
 slumpat_item = random.choice(lista)
+def kista():
+    kista=["Slangbella","Kannon","Gremlin"]
+    kista=random.randint(0,2)
+
