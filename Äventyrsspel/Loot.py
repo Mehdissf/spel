99ba061dbc8e6d5dec3fa3363552dfe579ad8201
Elliot <elliot.bruno@skola.taby.se>
2023-01-23 14:45:39 +0100

    rensat

--- Äventyrsspel/Loot.py
+++
@@ -1,6 +0,0 @@
-import random
-def loot():
-    loot = ["trolldryck", "gyllene svärd", "legendariska skölden"]
-    lootChnas = random.randint(0,2)
-    lootDrop = loot[lootChnas]
-    return lootDrop
